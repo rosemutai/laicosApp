72c14015088bdab998e595634d536f93f5e36da4
rosemutai <chepngetichrose2030@gmail.com>
2022-11-08 01:24:08 -0900

    add Login page to Home Page

---
+++ frontend/laicos/src/routes/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '../components/Login'
+
+const Home = () => {
+  return (
+    <div className='App w-full h-screen bg-[#FEFBE7] pt-12'>
+        <Login />
+    </div>
+  )
+}
+
+export default Home
