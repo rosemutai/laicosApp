991cdae541d04aed2bf83489c0f33c7e2e04ad2d
rosemutai <chepngetichrose2030@gmail.com>
2022-11-10 21:31:53 +0300

    add home page component

---
+++ frontend/laicos/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+
+// import Login from './components/Login';
+
+function App() {
+  return (
+    <div className='App w-full h-screen bg-[#FEFBE7] pt-12'>
+     {/* <Login /> */}
+    </div>
+  );
+}
+
+export default App;
