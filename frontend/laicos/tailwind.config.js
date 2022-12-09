dd02b62de4fba7c359517e9c71eea5607d161912
rosemutai <chepngetichrose2030@gmail.com>
2022-12-09 22:35:17 +0300

    add primary colors

--- frontend/laicos/tailwind.config.js
+++ frontend/laicos/tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '22': '22%',
+      },
+
+      colors: {
+        'purple': '#a76981',
+        'light-purple': '#F0DBDB',
+      },
+      
+    },
   },
   plugins: [],
 }
